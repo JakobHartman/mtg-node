c707c0a1e14ebecd9c2ae01b264fdd521fe9ca75
Jakobhartman <Jakobhartman@hotmail.com>
2015-08-04 07:24:38 -0500

    fixed back to doPost instead

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function getParams(urlText){
 //A sample POST request
 dispatcher.onPost('/card', function(req, res) {
 	var params = getParams(req.body);
-  console.log(params)
+  console.log("response body:" + req.body)
   res.end()
 	var card = params.text;
 	var channel = params.channel_name;
